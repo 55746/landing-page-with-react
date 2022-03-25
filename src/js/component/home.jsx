71e8b639d2efac385806f7c2cb70acaaaab27ba1
55746 <Lawhead.gavin@live.jobcorps.org>
2022-03-25 01:04:45 +0000

    almost finished fixing placement

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,15 +5,29 @@ import Footer from "./footer.jsx";
 import Card from "./card.jsx";
 //include images into your bundle
 //create your first component
-const Home = () => {
+const Home = (props) => {
+	let obj = {
+		// width: "60%",
+		// margin: "center",
+		justifyContent: "center",
+
+		// marginLeft: "181px",
+	};
 	return (
 		<div>
 			<Nav />
 			<Jumbo />
-			<Card />
+			<div className="card-group" style={obj}>
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</div>
 			<Footer />
 		</div>
 	);
 };
 
 export default Home;
+
+// src title Description. needs props
